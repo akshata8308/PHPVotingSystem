3a0f18ff012d2adb32861f6aae5f60736f47fe91
akshata8308 <akshatashinde4628@gmail.com>
2020-11-09 16:43:21 +0530

    PHP Project

---
+++ admin/canvassing.php
@@ -0,0 +1,407 @@
+
+<?php include ('session.php');?>
+<?php include ('head.php');?>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include ('side_bar.php');?>
+
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    
+					
+                </div>
+				
+				
+				
+				
+				<hr/>
+				
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+							<h4 class = "alert alert-success">Canvassing Report</h4>	
+                        </div>
+						
+					<br/>
+                       <form method="post" action="sort.php">
+			
+			<select name="position" id="position" class = "form-control pull-left" style = "width:300px;margin-left:19px; ">
+				<option readonly>----Sort by Position----</option>
+								<option>President</option>
+								<option>Vice president</option>
+								<option>Secretary</option>
+								<option>Treasurer</option>
+								<option>Documentation team head</option>
+								<option>Creative head</option>
+								<option>Technical head</option>
+								<option>PPT team head</option>
+								<option>Arrangment team</option>
+								<option>Presentation team</option>
+								<option>Overall team head</option>
+				
+			</select>
+			
+			&nbsp;
+			&nbsp;
+			<button id ="sort" class = "btn btn-success">Sort</button><button type="button" onclick="window.print();" style = "margin-right:14px;" id ="print" class = "pull-right btn btn-info"><i class = "fa fa-print"></i> Print</button>	
+</form>	
+                        <div class="panel-body">
+                            
+						
+			<table class="table table-striped table-bordered table-hover ">
+					<thead>
+						<td style = "width:600px;" class = "alert alert-success">Candidate for President</td>
+						<td style = "width:200px;"class = "alert alert-success">Image</td>
+						<td class = "alert alert-success">Total</td>
+					
+					</thead>
+					<?php
+		require 'dbcon.php';
+			$query = $conn->query("SELECT * FROM candidate WHERE position = 'President'");
+		while($fetch = $query->fetch_array())
+		{
+			$id = $fetch['candidate_id'];
+			$query1 = $conn->query("SELECT COUNT(*) as total FROM `votes` WHERE candidate_id = '$id'");
+			$fetch1 = $query1->fetch_assoc();
+			
+	?>
+					<tbody> 
+						<td><?php echo $fetch ['firstname']. " ".$fetch ['lastname'];?></td>
+						<td><img src = "<?php echo $fetch ['img'];?>" style = "width:40px; height:40px; border-radius:500px; " >
+						<td style = "width:20px; text-align:center"><button class = "btn btn-primary"disabled><?php echo $fetch1 ['total'];?></button>	</td>
+					<?php }?>
+					</tbody>
+					
+					
+			</table>
+
+			<table class="table table-striped table-bordered table-hover ">
+								<thead>
+									<td style = "width:600px;"class = "alert alert-success">Candidate for Vice President </td>
+									<td style = "width:200px;" class = "alert alert-success">Image</td>
+									<td class = "alert alert-success">Total</td>
+								
+								</thead>
+								<?php
+						require 'dbcon.php';
+						$query = $conn->query("SELECT * FROM candidate WHERE position = 'Vice President'");
+					while($fetch = $query->fetch_array())
+					{
+						$id = $fetch['candidate_id'];
+						$query1 = $conn->query("SELECT COUNT(*) as total FROM `votes` WHERE candidate_id = '$id'");
+						$fetch1 = $query1->fetch_assoc();
+						
+				?>
+					<tbody> 
+						<td><?php echo $fetch ['firstname']. " ".$fetch ['lastname'];?></td>
+						<td><img src = "<?php echo $fetch ['img'];?>" style = "width:40px; height:40px; border-radius:500px; " >
+						<td style = "width:20px; text-align:center"><button class = "btn btn-primary"disabled><?php echo $fetch1 ['total'];?></button>	</td>
+					<?php }?>
+					</tbody>
+					
+					
+			</table>		
+			
+			<table class="table table-striped table-bordered table-hover ">
+					<thead>
+						<td style = "width:600px;"class = "alert alert-success">Candidate for Secretary</td>
+						<td style = "width:200px;" class = "alert alert-success">Image</td>
+						<td class = "alert alert-success">Total</td>
+					
+					</thead>
+					<?php
+		require 'dbcon.php';
+		$query = $conn->query("SELECT * FROM candidate WHERE position = 'Secretary'");
+		while($fetch = $query->fetch_array())
+		{
+			$id = $fetch['candidate_id'];
+			$query1 = $conn->query("SELECT COUNT(*) as total FROM `votes` WHERE candidate_id = '$id'");
+			$fetch1 = $query1->fetch_assoc();
+			
+	?>
+					<tbody> 
+						<td><?php echo $fetch ['firstname']. " ".$fetch ['lastname'];?></td>
+						<td><img src = "<?php echo $fetch ['img'];?>" style = "width:40px; height:40px; border-radius:500px; " >
+						<td style = "width:20px; text-align:center"><button class = "btn btn-primary"disabled><?php echo $fetch1 ['total'];?></button>	</td>
+					<?php }?>
+					</tbody>
+					
+					
+			</table>		
+							
+
+			<table class="table table-striped table-bordered table-hover ">
+				<thead>
+					<td style = "width:600px;"class = "alert alert-success">Candidate for Treasurer</td>
+					<td style = "width:200px;" class = "alert alert-success">Image</td>
+					<td class = "alert alert-success">Total</td>
+					
+				</thead>
+			<?php
+		require 'dbcon.php';
+		$query = $conn->query("SELECT * FROM candidate WHERE position = 'Treasurer'");
+		while($fetch = $query->fetch_array())
+		{
+			$id = $fetch['candidate_id'];
+			$query1 = $conn->query("SELECT COUNT(*) as total FROM `votes` WHERE candidate_id = '$id'");
+			$fetch1 = $query1->fetch_assoc();
+			
+	?>
+					<tbody> 
+						<td><?php echo $fetch ['firstname']. " ".$fetch ['lastname'];?></td>
+						<td><img src = "<?php echo $fetch ['img'];?>" style = "width:40px; height:40px; border-radius:500px; " >
+						<td style = "width:20px; text-align:center"><button class = "btn btn-primary"disabled><?php echo $fetch1 ['total'];?></button>	</td>
+					<?php }?>
+					</tbody>
+					
+					
+			</table>	
+			
+			
+			
+			<table class="table table-striped table-bordered table-hover ">
+					<thead>
+						<td style = "width:600px;"class = "alert alert-success">Documentation team head</td>
+						<td style = "width:200px;" class = "alert alert-success">Image</td>
+						<td class = "alert alert-success">Total</td>
+					
+					</thead>
+					<?php
+		require 'dbcon.php';
+		$query = $conn->query("SELECT * FROM candidate WHERE position = 'Documentation team head'");
+		while($fetch = $query->fetch_array())
+		{
+			$id = $fetch['candidate_id'];
+			$query1 = $conn->query("SELECT COUNT(*) as total FROM `votes` WHERE candidate_id = '$id'");
+			$fetch1 = $query1->fetch_assoc();
+			
+	?>
+					<tbody> 
+						<td><?php echo $fetch ['firstname']. " ".$fetch ['lastname'];?></td>
+						<td><img src = "<?php echo $fetch ['img'];?>" style = "width:40px; height:40px; border-radius:500px; " >
+						<td style = "width:20px; text-align:center"><button class = "btn btn-primary"disabled><?php echo $fetch1 ['total'];?></button>	</td>
+					<?php }?>
+					</tbody>
+					
+					
+			</table>	
+			
+						
+			
+							
+			<table class="table table-striped table-bordered table-hover ">
+					<thead>
+						<td style = "width:600px;"class = "alert alert-success">Candidate for Creative team head</td>
+						<td style = "width:200px;" class = "alert alert-success">Image</td>
+						<td class = "alert alert-success">Total</td>
+					
+					</thead>
+					<?php
+		require 'dbcon.php';
+		$query = $conn->query("SELECT * FROM candidate WHERE position = 'Creative head'");
+		while($fetch = $query->fetch_array())
+		{
+			$id = $fetch['candidate_id'];
+			$query1 = $conn->query("SELECT COUNT(*) as total FROM `votes` WHERE candidate_id = '$id'");
+			$fetch1 = $query1->fetch_assoc();
+			
+	?>
+					<tbody> 
+						<td><?php echo $fetch ['firstname']. " ".$fetch ['lastname'];?></td>
+						<td><img src = "<?php echo $fetch ['img'];?>" style = "width:40px; height:40px; border-radius:500px; " >
+						<td style = "width:20px; text-align:center"><button class = "btn btn-primary"disabled><?php echo $fetch1 ['total'];?></button>	</td>
+					<?php }?>
+					</tbody>
+					
+			</table>	
+			
+							
+
+			
+							
+			<table class="table table-striped table-bordered table-hover ">
+					<thead>
+						<td style = "width:600px;"class = "alert alert-success">Candidate for Technical team head</td>
+						<td style = "width:200px;" class = "alert alert-success">Image</td>
+						<td class = "alert alert-success">Total</td>
+					
+					</thead>
+					<?php
+		require 'dbcon.php';
+		$query = $conn->query("SELECT * FROM candidate WHERE position = 'Technical head'");
+		while($fetch = $query->fetch_array())
+		{
+			$id = $fetch['candidate_id'];
+			$query1 = $conn->query("SELECT COUNT(*) as total FROM `votes` WHERE candidate_id = '$id'");
+			$fetch1 = $query1->fetch_assoc();
+			
+	?>
+					<tbody> 
+						<td><?php echo $fetch ['firstname']. " ".$fetch ['lastname'];?></td>
+						<td><img src = "<?php echo $fetch ['img'];?>" style = "width:40px; height:40px; border-radius:500px; " >
+						<td style = "width:20px; text-align:center"><button class = "btn btn-primary"disabled><?php echo $fetch1 ['total'];?></button>	</td>
+					<?php }?>
+					</tbody>
+					
+					
+			</table>	
+			
+							
+			<table class="table table-striped table-bordered table-hover ">
+					<thead>
+						<td style = "width:600px;"class = "alert alert-success">Candidate for PPT team head</td>
+						<td style = "width:200px;" class = "alert alert-success">Image</td>
+						<td class = "alert alert-success">Total</td>
+					
+					</thead>
+					<?php
+		require 'dbcon.php';
+		$query = $conn->query("SELECT * FROM candidate WHERE position = 'PPT team head'");
+		while($fetch = $query->fetch_array())
+		{
+			$id = $fetch['candidate_id'];
+			$query1 = $conn->query("SELECT COUNT(*) as total FROM `votes` WHERE candidate_id = '$id'");
+			$fetch1 = $query1->fetch_assoc();
+			
+	?>
+					<tbody> 
+						<td><?php echo $fetch ['firstname']. " ".$fetch ['lastname'];?></td>
+						<td><img src = "<?php echo $fetch ['img'];?>" style = "width:40px; height:40px; border-radius:500px; " >
+						<td style = "width:20px; text-align:center"><button class = "btn btn-primary"disabled><?php echo $fetch1 ['total'];?></button>	</td>
+					<?php }?>
+					</tbody>
+					
+					
+			</table>	
+			
+							
+			<table class="table table-striped table-bordered table-hover ">
+					<thead>
+						<td style = "width:600px;"class = "alert alert-success">Candidate for Arrangment team head</td>
+						<td style = "width:200px;" class = "alert alert-success">Image</td>
+						<td class = "alert alert-success">Total</td>
+					
+					</thead>
+					<?php
+		require 'dbcon.php';
+		$query = $conn->query("SELECT * FROM candidate WHERE position = 'Arrangment team'");
+		while($fetch = $query->fetch_array())
+		{
+			$id = $fetch['candidate_id'];
+			$query1 = $conn->query("SELECT COUNT(*) as total FROM `votes` WHERE candidate_id = '$id'");
+			$fetch1 = $query1->fetch_assoc();
+			
+	?>
+					<tbody> 
+						<td><?php echo $fetch ['firstname']. " ".$fetch ['lastname'];?></td>
+						<td><img src = "<?php echo $fetch ['img'];?>" style = "width:40px; height:40px; border-radius:500px; " >
+						<td style = "width:20px; text-align:center"><button class = "btn btn-primary"disabled><?php echo $fetch1 ['total'];?></button>	</td>
+					<?php }?>
+					</tbody>
+					
+					
+			</table>
+
+				
+			<table class="table table-striped table-bordered table-hover ">
+					<thead>
+						<td style = "width:600px;"class = "alert alert-success">Candidate for Presentation team head</td>
+						<td style = "width:200px;" class = "alert alert-success">Image</td>
+						<td class = "alert alert-success">Total</td>
+					
+					</thead>
+					<?php
+		require 'dbcon.php';
+		$query = $conn->query("SELECT * FROM candidate WHERE position = 'Presentation team'");
+		while($fetch = $query->fetch_array())
+		{
+			$id = $fetch['candidate_id'];
+			$query1 = $conn->query("SELECT COUNT(*) as total FROM `votes` WHERE candidate_id = '$id'");
+			$fetch1 = $query1->fetch_assoc();
+			
+	?>
+					<tbody> 
+						<td><?php echo $fetch ['firstname']. " ".$fetch ['lastname'];?></td>
+						<td><img src = "<?php echo $fetch ['img'];?>" style = "width:40px; height:40px; border-radius:500px; " >
+						<td style = "width:20px; text-align:center"><button class = "btn btn-primary"disabled><?php echo $fetch1 ['total'];?></button>	</td>
+					<?php }?>
+					</tbody>
+					
+					
+			</table>				
+			
+			
+			
+
+				
+			<table class="table table-striped table-bordered table-hover ">
+					<thead>
+						<td style = "width:600px;"class = "alert alert-success">Candidate for Overall team head</td>
+						<td style = "width:200px;" class = "alert alert-success">Image</td>
+						<td class = "alert alert-success">Total</td>
+					
+					</thead>
+					<?php
+		require 'dbcon.php';
+		$query = $conn->query("SELECT * FROM candidate WHERE position = 'Overall team head'");
+		while($fetch = $query->fetch_array())
+		{
+			$id = $fetch['candidate_id'];
+			$query1 = $conn->query("SELECT COUNT(*) as total FROM `votes` WHERE candidate_id = '$id'");
+			$fetch1 = $query1->fetch_assoc();
+			
+	?>
+					<tbody> 
+						<td><?php echo $fetch ['firstname']. " ".$fetch ['lastname'];?></td>
+						<td><img src = "<?php echo $fetch ['img'];?>" style = "width:40px; height:40px; border-radius:500px; " >
+						<td style = "width:20px; text-align:center"><button class = "btn btn-primary"disabled><?php echo $fetch1 ['total'];?></button>	</td>
+					<?php }?>
+					</tbody>
+					
+					
+			</table>				
+			
+			
+			
+			
+			
+			
+			
+			
+			
+							</div>				
+				
+							
+							
+							
+							
+							
+                          </div>
+                            <!-- /.table-responsive -->
+                            
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+              
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
+
+
+
+    </div>
+    <!-- /#wrapper -->
+
+    <?php include ('script.php');?>
+
+</body>
+
+</html>
+
